a034d49d713fc61dd7107ef4db1bb8951dddede2
Brand0nS <brandonscott@students.ptechnyc.org>
2020-06-23 17:10:37 -0400

    Bug #2 Fixed, and implementation of Dash Mechanic!
    I finally got the Jump to work! Awesome! By changing the height of the GroundCheck Object, it finally does proper collision for the PlayerObject, so now the boolean for isGrounded works. Also, started adding some rudimentary code for the dash mechanic.

--- MegaCloneProject/Assets/PlayerController2D.cs
+++ MegaCloneProject/Assets/PlayerController2D.cs
@@ -10,6 +10,12 @@ public class PlayerController2D : MonoBehaviour
     SpriteRenderer spriteRenderer;
     bool isGrounded;
     bool canMove;
+    public float doubleTapTime = 0.5f; //tapping the dash button within half a second will trigger this.
+    public float dashWaitTime = 2.0f; // must wait 2 seconds inbetween every dash.
+    // Time that the dash button was last pressed
+    private float _lastDashButtonTime;
+    // Time of the last dash
+    private float _lastDashTime;
     [SerializeField]
     Transform GroundCheck;
     // Start is called before the first frame update
@@ -21,13 +27,52 @@ public class PlayerController2D : MonoBehaviour
      
     }
 
+    bool canDash //can the character dash?
+    {
+        get
+        {
+            return Time.time - _lastDashTime > dashWaitTime;
+        }
+    }
+
+    void InitiateDash()
+    {
+        _lastDashTime = Time.time;
+        
+
+        // blah...
+    }
     // Update is called once per frame
-  
+
     private void FixedUpdate()
     {
-        if(Physics2D.Linecast(transform.position,GroundCheck.position, 1 << LayerMask.NameToLayer("Foreground")))
+        if (canDash && (Input.GetKey("d") || Input.GetKey("left")))
         {
-            isGrounded = true;
+            // If second time pressed?
+            if (Time.time - _lastDashButtonTime < doubleTapTime)
+            {
+                InitiateDash();
+                _lastDashButtonTime = Time.time;
+                rigidBody2D.velocity = new Vector2(4, rigidBody2D.velocity.y); //change velocity by 4
+                animator.Play("DashAnim");
+                spriteRenderer.flipX = true; // flip the character
+            }
+
+            else if (canDash && (Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.LeftArrow)))
+            {
+                if (Time.time - _lastDashButtonTime < doubleTapTime)
+                {
+                    InitiateDash();
+                    _lastDashButtonTime = Time.time;
+                    rigidBody2D.velocity = new Vector2(-4, rigidBody2D.velocity.y); //change velocity by -4
+                    animator.Play("DashAnim");
+                    spriteRenderer.flipX = false; //don't flip the character
+                }
+            }
+        }
+        if (Physics2D.Linecast(transform.position,GroundCheck.position, 1 << LayerMask.NameToLayer("Foreground"))) //if Linecast goes from player to foreground object,
+        {                                                                       //and it hits the Foreground Layer, then
+            isGrounded = true;                                                  //return true
         }
         else
         {
@@ -43,27 +88,35 @@ public class PlayerController2D : MonoBehaviour
                 animator.Play("WalkLoopAnim");
                 spriteRenderer.flipX = true; //flip the character
             }
+
+            if (Input.GetKey("right"))
+            {
+
+            }
             
 
         }
             else if (Input.GetKey("a") || Input.GetKey("left")) //else if key input is a or left arrow key
         {
             rigidBody2D.velocity = new Vector2(-2, rigidBody2D.velocity.y); //change velocity by -2 (go to the left)
-            
-            animator.Play("WalkLoopAnim");
-            spriteRenderer.flipX = false; //don't flip the character
+
+            if (isGrounded)
+            {
+                animator.Play("WalkLoopAnim");
+                spriteRenderer.flipX = false; //don't flip the character
+            }
         }
 
         else
         {
-            if (isGrounded)
+            if (isGrounded) //if the player is grounded, play the idle animation
             {
                 animator.Play("IdleAnim");
                 rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
             }
         }
 
-        if (Input.GetKey("space")&& isGrounded) //if key input is up or the spacebar key
+        if (Input.GetKey("space")&& isGrounded) //if the spacebar key is pressed and character is grounded
         {
             rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x,4); //keep the velocity the same for x but change it for y
             animator.Play("JumpAnim");
