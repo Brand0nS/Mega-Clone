c881a7d47d0de4552032ae2e40f6b010144526cc
Brand0nS <brandonscott@students.ptechnyc.org>
2020-06-23 02:31:35 -0400

    Edited PlayerController
    Finally got movement for arrow keys and keyboard. (Bug #1: Character Rotates when using playercontroller)

--- MegaCloneProject/Assets/PlayerController2D.cs
+++ MegaCloneProject/Assets/PlayerController2D.cs
@@ -22,12 +22,32 @@ public class PlayerController2D : MonoBehaviour
     {
         if (Input.GetKey("d") || Input.GetKey("right")) //if key input is d or the right arrow key
         {
-            rigidBody2D.velocity = new Vector2(2, 0); //change velocity by 2
+            rigidBody2D.velocity = new Vector2(2, rigidBody2D.velocity.y); //change velocity by 2
+            
+            animator.Play("WalkLoopAnim");
+            spriteRenderer.flipX = true; //flip the character
 
         }
             else if (Input.GetKey("a") || Input.GetKey("left")) //else if key input is a or left arrow key
         {
-            rigidBody2D.velocity = new Vector2(-2, 0); //change velocity by -2 (go to the left)
+            rigidBody2D.velocity = new Vector2(-2, rigidBody2D.velocity.y); //change velocity by -2 (go to the left)
+            
+            animator.Play("WalkLoopAnim");
+            spriteRenderer.flipX = false; //don't flip the character
+        }
+
+        else
+        {
+            
+            animator.Play("IdleAnim");
+        }
+
+        if (Input.GetKey("up") || Input.GetKey("space")) //if key input is up or the spacebar key
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x,4); //keep the velocity the same for x but change it for y
+            animator.Play("JumpAnim");
+            animator.Play("LandAnim");
+
         }
     }
 }
