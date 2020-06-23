9ba3f526fa0b60a19a22f3f2bfc828eb932bec99
Brand0nS <brandonscott@students.ptechnyc.org>
2020-06-23 10:59:02 -0400

    Broke Jump
    Fiixed some more animations, made a floor collide to try and fix to jump, but it didn't turn out so well lol. Bug#2 have to fix the Jump. Not sure if the animation is too slow, or what....

--- MegaCloneProject/Assets/PlayerController2D.cs
+++ MegaCloneProject/Assets/PlayerController2D.cs
@@ -8,25 +8,42 @@ public class PlayerController2D : MonoBehaviour
     Animator animator;
     Rigidbody2D rigidBody2D;
     SpriteRenderer spriteRenderer;
+    bool isGrounded;
+    bool canMove;
+    [SerializeField]
+    Transform GroundCheck;
     // Start is called before the first frame update
     void Start()
     {
-
        animator = GetComponent<Animator>();
         rigidBody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+     
     }
 
     // Update is called once per frame
   
     private void FixedUpdate()
     {
+        if(Physics2D.Linecast(transform.position,GroundCheck.position, 1 << LayerMask.NameToLayer("Foreground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        
+ 
         if (Input.GetKey("d") || Input.GetKey("right")) //if key input is d or the right arrow key
         {
             rigidBody2D.velocity = new Vector2(2, rigidBody2D.velocity.y); //change velocity by 2
+            if (isGrounded) 
+            {
+                animator.Play("WalkLoopAnim");
+                spriteRenderer.flipX = true; //flip the character
+            }
             
-            animator.Play("WalkLoopAnim");
-            spriteRenderer.flipX = true; //flip the character
 
         }
             else if (Input.GetKey("a") || Input.GetKey("left")) //else if key input is a or left arrow key
@@ -39,15 +56,18 @@ public class PlayerController2D : MonoBehaviour
 
         else
         {
-            
-            animator.Play("IdleAnim");
+            if (isGrounded)
+            {
+                animator.Play("IdleAnim");
+                rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
+            }
         }
 
-        if (Input.GetKey("up") || Input.GetKey("space")) //if key input is up or the spacebar key
+        if (Input.GetKey("space")&& isGrounded) //if key input is up or the spacebar key
         {
             rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x,4); //keep the velocity the same for x but change it for y
             animator.Play("JumpAnim");
-            animator.Play("LandAnim");
+            
 
         }
     }
