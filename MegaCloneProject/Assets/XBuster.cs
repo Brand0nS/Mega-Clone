3f078e4117971b8cdf8e5ff33239ec375104d9e3
Brand0nS <brandonscott@students.ptechnyc.org>
2020-06-23 21:38:09 -0400

    Added Shooting Mechanics
    Added Bullet Prefab, and the ability to shoot. Will add the charge shot ability later.

---
+++ MegaCloneProject/Assets/XBuster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XBuster : MonoBehaviour
+{
+    public float bulletSpeed = 3f;
+    public Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D.velocity = transform.right * bulletSpeed;
+    }
+
+    
+}
